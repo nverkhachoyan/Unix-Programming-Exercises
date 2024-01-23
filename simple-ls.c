ab3e09eb15403feb19628277fd58a80320f57089
Nver Khachoyan <khachoyannver@gmail.com>
2024-01-22 19:08:31 -0800

    simple ls

---
+++ simple-ls.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+int
+main(int argc, char *argv[]) {
+	DIR *dp;
+	struct dirent *dirp;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: ls path\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((dp = opendir(argv[1])) == NULL) {
+		fprintf(stderr, "Failed to open directory.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((dirp = readdir(dp)) != NULL) {
+		printf("%s\n", dirp->d_name); 	
+	}
+
+
+	return 0;
+}
